4faea035f7a0a9917eb97536adae7534e821cbe8
Yhefritd <huachomontes123.1@gmail.com>
2025-07-31 13:00:38 -0500

    Agregando funcionalidad y metodos como TDD y CI

--- Buscador_Pelicula/urls.py
+++ Buscador_Pelicula/urls.py
@@ -1,7 +1,9 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),              
-    path('inicio/', views.pagina_index, name='inicio')  
+    path('', views.index, name='index'),
+    path('peliculas/', views.index, name='peliculas'),
+    path('series/', views.series, name='series'),
 ]
--- Buscador_Pelicula/views.py
+++ Buscador_Pelicula/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'taquilla.html')
+    return render(request, 'Peliculas.html')
 
 def pagina_index(request):
-    return render(request, 'index.html')
+    return render(request, 'Peliculas.html')
+
+def series(request):
+    return render(request, 'Series.html')
